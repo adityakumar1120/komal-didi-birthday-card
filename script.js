12eb872b8430d09f745b99276fcbb60efe111a79
adityakumar1120 <adityaakumar1120@gmail.com>
2025-01-19 21:42:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const btn = document.querySelector('.btn')
+const flameWrapper = document.querySelector('.flame-wrapper')
+const containerElem = document.querySelector('.container')
+const popupElem = document.querySelector('.popup-cont')
+const main = document.querySelector('main')
+const snowFallAnimation = document.querySelector('.snow-ball-container')
+let audio = new Audio
+audio.src = `/audios/perfect song.unknown`
+audio.preload = 'auto'
+
+btn.addEventListener('click' , ()=>{
+    flameWrapper.classList.add('hidden')
+    btn.style.background = `linear-gradient(135deg, #f63b26 10%, #F5AF19 100%)`
+    setTimeout(()=>{
+        containerElem.children[0].classList.add('hidden')
+        containerElem.children[1].classList.add('hidden')
+        setTimeout(()=>{
+            containerElem.classList.add('none')
+            popupElem.classList.remove('none')
+        },100)
+    },700)
+    setTimeout(()=>{
+            audio.play()
+            snowFallAnimation.innerHTML = `   <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>
+      <div class="snow-ball"></div>`
+    },1000)
+})
+
+function getWindowInnerWidth(){
+    const windowInnerheight = window.outerHeight
+    main.style.height = `${windowInnerheight}px`
+}
+
+window.addEventListener('resize', (e)=>{
+    getWindowInnerWidth()
+})
+
+const receiverName = document.querySelector("#receiver");
+const senderName = document.querySelector("#sender");
+receiverName.innerText = new URLSearchParams(location.search).get("receiver")|| 'Komal DiDi';
+senderName.innerText = new URLSearchParams(location.search).get("sender") || 'Aditya kumar';
